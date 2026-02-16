471b2032a3d55c4c43e25780a3f9804edb2131e6
FFucks <fabio_geiss@hotmail.com>
2026-02-16 09:16:40 -0300

    presentation examples and images

--- src/main/java/com/ffucks/ContactApp.java
+++ src/main/java/com/ffucks/ContactApp.java
@@ -18,6 +18,9 @@ public class ContactApp extends Application {
 
         Scene scene = new Scene(loader.load(), 800, 600);
 
+        /*scene.getStylesheets().add(
+                getClass().getResource("/view/style.css").toExternalForm());*/
+
         stage.setTitle("Contact Manager");
         stage.setScene(scene);
         stage.show();
